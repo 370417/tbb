5c78a75a885984ecec270e14324bb39fd022c0a8
Albert Ford <albert@albertford.com>
2022-08-15 18:52:24 -0700

    Move date functions to separate module

---
+++ src/date.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use time::{macros::format_description, Date, OffsetDateTime};
+
+/// Get the current local date, or get the date from $TBB_DEFAULT_DATE if it exsits.
+pub fn init_date() -> Result<Date> {
+    std::env::var("TBB_DEFAULT_DATE")
+        .map_err(anyhow::Error::from)
+        .and_then(parse_numerical_date)
+        .or_else(|_| get_today())
+}
+
+pub fn get_today() -> Result<Date> {
+    OffsetDateTime::now_local()
+        .map(OffsetDateTime::date)
+        .map_err(anyhow::Error::from)
+}
+
+pub fn parse_numerical_date<StrRef: AsRef<str>>(date_str: StrRef) -> Result<Date> {
+    let format = format_description!("[year]-[month]-[day]");
+    Date::parse(date_str.as_ref(), &format).map_err(anyhow::Error::from)
+}
+
+pub fn format_month_year(date: &Date) -> Result<String> {
+    let format = format_description!("[month repr:short] [year]");
+    date.format(&format).map_err(anyhow::Error::from)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod date;
+
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use time::{macros::format_description, Date, OffsetDateTime};
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let today = init_date()?;
+    let today = date::init_date()?;
 
     match args.command {
-        Commands::Status => println!("[ {} ]", format_month_year(&today)?),
+        Commands::Status => println!("[ {} ]", date::format_month_year(&today)?),
     }
 
     Ok(())
@@ -27,28 +28,4 @@ struct Args {
 enum Commands {
     /// Show one month's budget
     Status,
-}
-
-/// Get the current local date, or get the date from $TBB_DEFAULT_DATE if it exsits.
-fn init_date() -> Result<Date> {
-    std::env::var("TBB_DEFAULT_DATE")
-        .map_err(anyhow::Error::from)
-        .and_then(parse_numerical_date)
-        .or_else(|_| get_today())
-}
-
-fn get_today() -> Result<Date> {
-    OffsetDateTime::now_local()
-        .map(OffsetDateTime::date)
-        .map_err(anyhow::Error::from)
-}
-
-fn parse_numerical_date<StrRef: AsRef<str>>(date_str: StrRef) -> Result<Date> {
-    let format = format_description!("[year]-[month]-[day]");
-    Date::parse(date_str.as_ref(), &format).map_err(anyhow::Error::from)
-}
-
-fn format_month_year(date: &Date) -> Result<String> {
-    let format = format_description!("[month repr:short] [year]");
-    date.format(&format).map_err(anyhow::Error::from)
 }
